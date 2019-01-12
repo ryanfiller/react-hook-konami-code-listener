c2ab1d8b1a8900f8479a32c7ad366d96491e18fd
Ryan Filler <ryanfiller89@gmail.com>
2019-01-12 17:18:13 -0600

    oops

--- src/App.js
+++ src/App.js
@@ -4,53 +4,53 @@ import './App.css';
 
 import useKonamiListener from './useKonamiListener';
 
-// const App = (props) => {
-
-//   const [match, keystrokes] = useKonamiListener();
-
-//   return (
-//     <div className="App" >
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Enter the <a
-//             className="App-link"
-//             href="https://en.wikipedia.org/wiki/Konami_Code"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Konami Code
-//           </a>
-//         </p>
-//         <p>
-//           <kbd>↑</kbd>
-//           <kbd>↑</kbd>
-//           <kbd>↓</kbd>
-//           <kbd>↓</kbd>
-//           <kbd>←</kbd>
-//           <kbd>→</kbd>
-//           <kbd>←</kbd>
-//           <kbd>→</kbd>
-//           <kbd>B</kbd>
-//           <kbd>A</kbd>
-//         </p>
-//       </header>
-//       {console.log(keystrokes)}
-//       {match === true ? <div className="popup"><h2>You did it!</h2><p>Refresh the page.</p></div> : null}
-//     </div>
-//   )
-// }
-
 const App = (props) => {
 
   const [match, keystrokes] = useKonamiListener();
 
   return (
-    <div data-testid="listener">
-      {keystrokes.join()}
-      {match === true ? 'true' : null}
+    <div className="App" >
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Enter the <a
+            className="App-link"
+            href="https://en.wikipedia.org/wiki/Konami_Code"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Konami Code
+          </a>
+        </p>
+        <p>
+          <kbd>↑</kbd>
+          <kbd>↑</kbd>
+          <kbd>↓</kbd>
+          <kbd>↓</kbd>
+          <kbd>←</kbd>
+          <kbd>→</kbd>
+          <kbd>←</kbd>
+          <kbd>→</kbd>
+          <kbd>B</kbd>
+          <kbd>A</kbd>
+        </p>
+      </header>
+      {console.log(keystrokes)}
+      {match === true ? <div className="popup"><h2>You did it!</h2><p>Refresh the page.</p></div> : null}
     </div>
   )
 }
+
+// const App = (props) => {
+
+//   const [match, keystrokes] = useKonamiListener();
+
+//   return (
+//     <div data-testid="listener">
+//       {keystrokes.join()}
+//       {match === true ? 'true' : null}
+//     </div>
+//   )
+// }
 
 export default App;
