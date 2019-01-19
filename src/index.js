9605c03bbfaf5d2d3d715ab5b3adc54b5a92007b
Ryan Filler <ryanfiller89@gmail.com>
2019-01-19 15:43:58 -0600

    corrects path to sourcefile, bumps version to .4

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import useKonamiListener from './useKonamiListener';
 
-// const App = () => {
+const App = () => {
 
-//   const [match, keystrokes] = useKonamiListener();
+  const [match, keystrokes] = useKonamiListener();
 
-//   return (
-//     <div data-testid="listener">
-//       {keystrokes.join()}
-//       {match === true ? 'true' : null}
-//     </div>
-//   )
-// }
+  return (
+    <div data-testid="listener">
+      {keystrokes.join()}
+      {match === true ? 'true' : null}
+    </div>
+  )
+}
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-export default useKonamiListener;
+ReactDOM.render(<App />, document.getElementById('root'));
