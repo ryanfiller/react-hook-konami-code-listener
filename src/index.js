f519de976ec588f9c7528a3efe0c04e3b1a7f16c
Ryan Filler <ryanfiller89@gmail.com>
2019-01-19 14:51:07 -0600

    remove unused stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import useKonamiListener from './useKonamiListener';
+
+const App = () => {
+
+  const [match, keystrokes] = useKonamiListener();
+
+  return (
+    <div data-testid="listener">
+      {keystrokes.join()}
+      {match === true ? 'true' : null}
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
